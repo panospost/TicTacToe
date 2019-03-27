47d195fe7f93593c52952219ff1cbfc0c6934bb9
panospost <panospost1@gmail.com>
2019-03-27 14:51:23 +0200

    migrate to androidx

--- app/src/androidTest/java/com/post/ppc/tictactoe/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/post/ppc/tictactoe/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.post.ppc.tictactoe
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
